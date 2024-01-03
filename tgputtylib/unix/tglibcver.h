69d93621972bab3ec17c3dc82e3788cdfcedb972
Tobias Giesen <tobias@tgtools.de>
2024-01-03 01:19:16 +0100

    Update tglibcver.h

--- tgputtylib/unix/tglibcver.h
+++ tgputtylib/unix/tglibcver.h
@@ -2,8 +2,9 @@
 
 TG
 
-On some architectures, we need to specify which memcpy version to use (from which glibc)
-because we need to ensure binary compatibility with various Linux distributions 
+On some architectures, we need to replace memcpy with memmove, or
+specify which memcpy version to use (from which glibc)
+because we need to ensure binary compatibility with various Linux distributions
 (including some NAS operating systems).
 
 To list available versions (example for an i386 Ubuntu machine):
@@ -12,10 +13,22 @@ objdump -TC /lib/i386-linux-gnu/libc.so.6 | grep memcpy
 To see gcc compiler predefines:
 echo | gcc -dM -E -
 
+*******************************************
+
+Another problem is if the whole GLIBC version is too new. For example:
+TGPuttyLibLoadError: /lib/libc.so.6: version `GLIBC_2.28' not found
+
+In that case, you need to find an older Linux development system.
+
+*******************************************
+
 */
+
 #ifndef tglibcver___H
 #define tglibcver___H
+
 #if __GNUC__ > 5
+
 #ifdef i386
 __asm__(".symver memcpy,memcpy@GLIBC_2.0");
 #endif
@@ -24,6 +37,10 @@ __asm__(".symver memcpy,memcpy@GLIBC_2.0");
 __asm__(".symver memcpy,memcpy@GLIBC_2.2.5");
 #endif
 
+// this is easier/better than specifying a GLIBC version, but we'll do both to be really safe:
+
+#define memcpy memmove
+
 #else
 
 // older GNU compiler (__GNUC__ <= 5)
@@ -31,3 +48,5 @@ __asm__(".symver memcpy,memcpy@GLIBC_2.2.5");
 
 #endif
 #endif
+
+
